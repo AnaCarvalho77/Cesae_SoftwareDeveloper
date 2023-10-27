395bb182902f86166ccbed0c1bcc42debf2edeb0
Ana Sofia Carvalho <ascarvalho77@gmail.com>
2023-10-27 14:44:36 +0100

    atual

--- FichasJava/src/FichaPratica_03/Ex_14.java
+++ FichasJava/src/FichaPratica_03/Ex_14.java
@@ -7,30 +7,30 @@ public class Ex_14 {
         //Instanciar o Scanner
         Scanner input = new Scanner (System.in);
 
-      /*  //Declarar variáveis
-        int num, aux = 0;
-
-        //Ler numero
-        System.out.print("Fatorial de: ");
-        num = input.nextInt();
-        aux = num -1;
-
-        //Pedir números
-
-
-        if(num > 0){
-            while(aux >= 1 ){
-                num *= aux;
-                aux--;
-            }
-        }else {
-            System.out.println("numero inválido");
-        }
-        System.out.println("Factorial = " + num);*/
+//        //Declarar variáveis
+//        int num, aux = 0;
+//
+//        //Ler numero
+//        System.out.print("Fatorial de: ");
+//        num = input.nextInt();
+//        aux = num -1;
+//
+//        //Pedir números
+//
+//
+//        if(num > 0){
+//            while(aux >= 1 ){
+//                num *= aux;
+//                aux--;
+//            }
+//        }else {
+//            System.out.println("numero inválido");
+//        }
+//        System.out.println("Factorial = " + num);
 
 
         //Declarar variáveis
-        int numIni,num, aux = 0, i, numSoma = 0, j;
+        int numIni,num, aux, i, numSoma, j;
 
         //Ler numero
         System.out.print("Fatorial de: ");
