7b0cc877d66fb29c1598e6ba83312755df72b68e
Ana Sofia Carvalho <ascarvalho77@gmail.com>
2023-11-09 10:49:42 +0000

    atual

--- FichasJava/src/Ficha_Extra_Funcoes/Ex_06.java
+++ FichasJava/src/Ficha_Extra_Funcoes/Ex_06.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 public class Ex_06 {
     static double somatorio(int num){
 
-        int soma = 0, i;
+        int soma = 0, i,cont = 0, numVetor, numAtual;
+
+
+        numAtual = num;
 
 //        String numero = Integer.toString(num);
 //        char[]  arrayNumero = numero.toCharArray();
@@ -19,6 +22,57 @@ public class Ex_06 {
 //            soma += array[i];
 //        }
 
+        while (numAtual >= 10) {
+            if (numAtual % 10 != 0) {
+                numVetor = num % 10;
+                numAtual = num - numVetor;
+                num = numAtual;
+                cont++;
+
+            } else {
+                while (numAtual >= 10 || numAtual <= -10) {
+                     numAtual = numAtual / 10;
+                    if(numAtual < 10){
+                        numVetor = numAtual;
+                        cont++;
+                    }else {
+                        numVetor = numAtual % 10;
+                        if (numVetor == 0) {
+                            numAtual = 0;
+                            cont++;
+                        } else {
+                            numAtual = numAtual - numVetor;
+                            cont++;
+                        }
+                    }
+                }
+            }
+        }
+
+        int [] vetor = new int[cont];
+
+        for(i = 0; i < vetor.length; i++) {
+            if (numAtual % 10 != 0) {
+                numVetor = num % 10;
+                vetor[i] = numVetor;
+                numAtual = num - numVetor;
+
+            } else {
+                numAtual = numAtual / 10;
+
+                if (numAtual >= 10 || numAtual <= -10) {
+                    numVetor = numAtual % 10;
+                    vetor[i] = numVetor;
+                    numAtual = numVetor * 10;
+                }else{
+                    vetor [i] = numAtual;
+                }
+            }
+        }
+
+        for(i = 0; i < vetor.length; i++){
+           soma +=  vetor[i];
+        }
         
         return soma;
     }
@@ -29,7 +83,7 @@ public class Ex_06 {
 
         int num;
 
-        System.out.print("Digitte um número: ");
+        System.out.print("Introduza um número: ");
         num = input.nextInt();
 
         System.out.println(somatorio(num));
