de0044c7c0f2a4f79a48fb5d9b3b037accb03720
Ana Sofia Carvalho <ascarvalho77@gmail.com>
2023-11-14 16:35:34 +0000

    atual

--- FichasJava/src/FichaPratica_07/Ex_11.java
+++ FichasJava/src/FichaPratica_07/Ex_11.java
@@ -5,24 +5,61 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Ex_11 {
-    public static void pesquisarMusicaGenero(String [][] matriz, String genero){
-       String[] musicasGenero = new String[matriz.length];
-       int indexAtual=0;
-        for(int i= 0; i< matriz.length; i++){
-            if(matriz[i][2].equals(genero) || !matriz[i][2].equals(null)){
-                musicasGenero[indexAtual]= matriz[i][0];
+
+    public static void pesquisarMusicaGenero(String[][] matriz, String genero) {
+        String[] musicasGenero = new String[matriz.length];
+        int indexAtual = 0;
+        for (int i = 0; i < matriz.length; i++) {
+            if (matriz[i][2].equals(genero) ) {
+                musicasGenero[indexAtual] = matriz[i][0];
                 indexAtual++;
             }
         }
-        for(int i=0; i< musicasGenero.length; i++){
+        for (int i = 0; i < musicasGenero.length; i++) {
+            if (musicasGenero[i] != null) {
                 System.out.println(musicasGenero[i]);
             }
+        }
     }
+
+    public static void pesquisarMusicaArtista(String[][] matriz, String artista) {
+        String[] musicasArtista = new String[matriz.length];
+        int indexAtual = 0;
+        for (int i = 0; i < matriz.length; i++) {
+            if (matriz[i][1].equals(artista)) {
+                musicasArtista[indexAtual] = matriz[i][0];
+                indexAtual++;
+            }
+        }
+        for (int i = 0; i < musicasArtista.length; i++) {
+            if (musicasArtista[i] != null) {
+                System.out.println(musicasArtista[i]);
+            }
+
+        }
+    }
+
+    public static void musicaMaior(String[][] matriz) {
+        String maior,linha;
+        int indexAtual = 0;
+        for (int i = 0; i < matriz.length; i++) {
+                linha = matriz[i][3];
+                indexAtual++;
+            }
+        }
+        for (int i = 0; i < musicasArtista.length; i++) {
+            if (!musicasArtista[i].equals(null)) {
+                System.out.println(musicasArtista[i]);
+            }
+
+        }
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
 
         Scanner fichOrigem = new Scanner(new File("Ficheiros/exercicio_11.csv"));
 
-        int contLinhas = 0, contColunas=0;
+        int contLinhas = 0, contColunas = 0;
         while (fichOrigem.hasNextLine()) {
             String linha = fichOrigem.nextLine();
             String[] itens = linha.split(",");
@@ -33,26 +70,26 @@ public class Ex_11 {
         fichOrigem.close();
         Scanner fichOrigem2 = new Scanner(new File("Ficheiros/exercicio_11.csv"));
 
-        String nomeMusica, Artista, genero, opcao;
+        String nomeMusica, artista, genero, opcao;
         int horas, minutos, nLinha = 0;
 
-        String[][] matriz = new String[contLinhas-1][ contColunas];
+        String[][] matriz = new String[contLinhas - 1][contColunas];
         String linha = fichOrigem2.nextLine();
 
         while (fichOrigem2.hasNextLine()) {
             linha = fichOrigem2.nextLine();
             String[] itens = linha.split(",");
 
-            for(int i=0; i< itens.length; i++){
+            for (int i = 0; i < itens.length; i++) {
                 matriz[nLinha][i] = itens[i];
             }
-            nLinha ++;
+            nLinha++;
 
         }
 
-        for(int i=0; i< matriz.length; i++){
+        for (int i = 0; i < matriz.length; i++) {
             System.out.println();
-            for(int j = 0; j< matriz[0].length;j++){
+            for (int j = 0; j < matriz[0].length; j++) {
                 System.out.print(matriz[i][j]);
             }
         }
@@ -67,25 +104,26 @@ public class Ex_11 {
         opcao = input.next();
 
 
-
-
-
-        switch (opcao){
+        switch (opcao) {
             case "1":
                 System.out.println("Qual o género: ");
                 genero = input.next();
-                pesquisarMusicaGenero(matriz,genero);
+                pesquisarMusicaGenero(matriz, genero);
                 break;
 
-                case
+            case "2":
+                System.out.println("Qual o artista: ");
+                artista = input.next();
+                pesquisarMusicaArtista(matriz, artista);
+                break;
+
+//            case"3":
+
             default:
                 System.out.println("Erro!número opção não contemplado.");
 
 
-
         }
-
-
 
 
     }
