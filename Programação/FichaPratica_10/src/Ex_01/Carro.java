c1d601e0edf22efb92d16cba3b3478bbb6c0c0ef
Ana Sofia Carvalho <ascarvalho77@gmail.com>
2023-11-20 15:25:46 +0000

    atual

--- Programação/FichaPratica_10/src/Ex_01/Carro.java
+++ Programação/FichaPratica_10/src/Ex_01/Carro.java
@@ -1,55 +1,89 @@
 package Ex_01;
 
 public class Carro {
-        private String marca;
-        private String modelo;
-        private int anoFabrico;
-        private double potencia;
-        private double cilindrada;
-        private double consumo;
-        private TipoCombustivel tipoCombustivel;
+    private String marca;
+    private String modelo;
+    private int anoFabrico;
+    private double potencia;
+    private double cilindrada;
+    private double consumo;
+    private TipoCombustivel tipoCombustivel;
 
-        public Carro(String marca, String modelo, int ano) {
-            this.marca = marca;
-            this.modelo = modelo;
-            this.anoFabrico = ano;
 
+    public Carro(String marca, String modelo, int anoFabrico, double potencia, double cilindrada, double consumo, TipoCombustivel tipoCombustivel) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anoFabrico = anoFabrico;
+        this.potencia = potencia;
+        this.cilindrada = cilindrada;
+        this.consumo = consumo;
+        this.tipoCombustivel = tipoCombustivel;
+    }
+
+    public void ligar() {
+
+        System.out.println("O carro está ligado!");
+    }
+
+    public void ligar(int anoAtual) {
+        if (anoAtual - this.anoFabrico > 30) {
+            if (this.tipoCombustivel.equals(TipoCombustivel.GASOLEO)) {
+                System.out.println("Deita um pouco de fumo… Custa a pegar… O carro está ligado!\nVrum-vrum-vrum");
+            } else {
+                System.out.println("Custa a pegar… O carro está ligado!\nVrum-vum-vrum");
+            }
+
+        } else {
+            if (this.potencia < 250) {
+                System.out.println("O carro está ligado!\nVruummmmmmm");
+            } else {
+                System.out.println("O carro está ligado!\nVRUUMMMMMM");
+            }
         }
+    }
 
-
-
-        public void ligar(){
-
-            System.out.println("O carro está ligado!");
-        }
-
-        public void ligar(int anoAtual){
-           if(this.anoFabrico > (anoAtual - this.anoFabrico)) {
-                if(this.tipoCombustivel.equals("Gasoleo")){
-                    System.out.println("Deita um pouco de fumo… Custa a pegar… O carro está ligado! Vrum-vrum-vrum");
-                }else{
-                    System.out.println("Custa a pegar… O carro está ligado! Vrum-vum-vrum");
-                }
-
-            }else{
-               if(this.potencia < 250){
-                   System.out.println("O carro está ligado! Vruummmmmmm");
-               }else {
-                   System.out.println("O carro está ligado! VRUUMMMMMM");
-               }
-           }
-        }
-
-        public Carro corrida(Carro carro, int anoAtual){
-            if(carro.potencia > this.potencia){
-                if(carro.cilindrada > this.cilindrada ){
-                    if(ano )
-                }else if(carro.cilindrada < this.cilindrada){
+    public Carro corrida(Carro carro, int anoAtual) {
+        if (carro.potencia > this.potencia) {
+            return carro;
+        } else if (carro.potencia < this.potencia) {
+            return this;
+        } else if (carro.potencia == this.potencia) {
+            if (carro.cilindrada > this.cilindrada) {
+                return carro;
+            } else if (carro.cilindrada < this.cilindrada) {
+                return this;
+            } else if (carro.cilindrada == this.cilindrada) {
+                if (anoAtual - carro.anoFabrico > anoAtual - this.anoFabrico) {
+                    return carro;
+                } else if (anoAtual - carro.anoFabrico < anoAtual - this.anoFabrico) {
                     return this;
                 }
-            }else if (carro.potencia < this.potencia){
-                return this;
+
             }
         }
 
+        return null;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public double valorConsumido(double distancia){
+        double valorConsumido = (distancia/100) * this.consumo;
+        return valorConsumido;
+
+    }
+
+public void exibirDetalhes(){
+    System.out.println(this.marca);
+    System.out.println(this.modelo);
+    System.out.println(this.anoFabrico);
+    System.out.println(this.potencia);
+    System.out.println(this.cilindrada);
 }
+
+
+}
+
+
--- Programação/FichaPratica_10/src/Ex_01/main.java
+++ Programação/FichaPratica_10/src/Ex_01/main.java
@@ -1,4 +1,31 @@
 package Ex_01;
 
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        Carro carro1 = new Carro("Mercedes","A45",2023,150, 1800, 7, TipoCombustivel.GASOLEO);
+        Carro carro2 = new Carro("Toyota","Land Cruiser",2021,200, 2000, 15, TipoCombustivel.GASOLINA);
+        Carro carro3 = new Carro("Ferrari","SF90",2023,800, 5000, 25, TipoCombustivel.GASOLINA);
+        Carro carro4 = new Carro("Toyota","Rav4",2021,180, 1800, 15, TipoCombustivel.GASOLINA);
+
+
+        Carro vencedorCorrida1 = carro1.corrida(carro2, 2023);
+        Carro vencedorCorrida2 = carro3.corrida(carro4, 2023);
+      
+        Carro vencedorFinal = vencedorCorrida1.corrida(vencedorCorrida2,2023);
+        System.out.println("O vencedor é:");
+     vencedorFinal.exibirDetalhes();
+
+
+    }
+
+
+
+
+
+
 }
