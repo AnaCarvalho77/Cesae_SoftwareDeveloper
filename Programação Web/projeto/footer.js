a30bd12d6169c411e4d9101bdb218ec3af7d54ad
Ana Sofia Carvalho <ascarvalho77@gmail.com>
2023-12-30 20:28:10 +0000

    atual

--- Programação Web/projeto/footer.js
+++ Programação Web/projeto/footer.js
@@ -2,7 +2,8 @@
 class Myfooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        
+        <p><a href="mailto:ibertrio@gmail.com" target="_blank">ibertrio@gmail.com</a></p>
+        <br>
         <p>© Ibertrio</p>
         `
     }
