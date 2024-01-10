c0b9b4c854079bf242a9d5a782f24b9a02ee5fea
Ana Sofia Carvalho <ascarvalho77@gmail.com>
2024-01-10 08:59:04 +0000

    atual

---
+++ Programação Web/TFSoftDev_SofiaCarvalho_/javascript/header.js
@@ -0,0 +1,11 @@
+// header.js
+class Myheader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+        <div id="header">
+          <a href="https://www.facebook.com/Ibertrio" target="_blank"> <img src="/imagens/facebook.png" alt="icon facebook"></a>
+        </div>
+        `
+  }
+}
+window.customElements.define('my-header', Myheader)
