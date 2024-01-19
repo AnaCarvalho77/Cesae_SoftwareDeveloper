361a06f8ff45e777853263b9403c499651950420
Ana Sofia Carvalho <ascarvalho77@gmail.com>
2024-01-19 16:53:30 +0000

    19janeiro

--- IOS/03_PrimeiraApp/03_PrimeiraApp/ViewController.swift
+++ IOS/03_PrimeiraApp/03_PrimeiraApp/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var image: UIImageView!
+    var imagens:[UIImage]=[UIImage(named:"fox")!, UIImage(named: "lion")!,UIImage(named:"monkey")!,UIImage(named:"pig")!]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func butaoClicado(_ sender: UIButton) {
+//        print("Botão Clicado!")
+//        image.image = UIImage(named: "lion")
+    
+        var i = Int.random(in: 0...3)
+        let imagemSelecionda = imagens[i]
+        
+        image.image = imagemSelecionda
+    }
+    
 }
 
