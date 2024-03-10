5aa1b5ffaee924652129387e251ff5a0d5ca003a
Ana Sofia Carvalho <ascarvalho77@gmail.com>
2024-03-10 22:42:39 +0000

    atual

--- Programação/teste/src/Main.java
+++ Programação/teste/src/Main.java
@@ -2,5 +2,17 @@
 public class Main {
     public static void main(String[] args) {
 
+        String[] cores = {"amarelo", "vermelho", "azul", "verde", "rosa"};
+        int[] quantidade={2, 5, 0, 6,1};
+
+        for(int cor=0; cor < cores.length;cor++){
+                int quantAtual = quantidade[cor];
+                while(quantAtual>0){
+                    System.out.println(cores[cor]);
+                    quantAtual--;
+                }
+
+            }
         }
+        
     }
